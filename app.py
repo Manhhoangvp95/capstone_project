a8a5233ba158e0f5a3e66adbe63f4e9876cd0f00
Hoang Manh <manhhoang.vp95@gmail.com>
2022-12-19 20:10:04 +0700

    Add docker backup

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Hello World, My Name is Hoang Manh2!</h1>'
+    return '<h1>Hello World, My Name is Hoang Manh!</h1>'
 
 app.run(host='0.0.0.0', port=4000)
