1f5ab40db7f397f448b4fd7f73f7df4bf15f10b0
Hoang Manh <manhhoang.vp95@gmail.com>
2022-12-19 20:06:32 +0700

    Add docker backup

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+# Integrate kubectl with aws eks
 aws eks update-kubeconfig --region us-east-1 --name ${cluster_name}
 
 export KUBECONFIG=~/.kube/config
-
-kubectl get nodes
-
-kubectl apply -f flask_deployment.yaml
+# Set variable to config file
+cat flask_deployment.yaml | envsubst '${CIRCLE_SHA1}' > flask_config_deployment.yaml
+# Deploy
+kubectl apply -f flask_config_deployment.yaml
